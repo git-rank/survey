a584f3919aee3bc7cc1df84f6753d27603071590
Alexis Schad <schadoc_alex@hotmail.fr>
2014-10-21 01:08:58 +0200

    Création d'un design simple avec Bootstrap

--- survey.php
+++ survey.php
@@ -1,7 +1,7 @@
 <?php
 	include_once("MySQL.class.php");
 
-	// Connexion to the database
+	// Connect to the database
 	$db = MySQL::getInstance();
 
 	// Identify the visitor
@@ -19,8 +19,55 @@
 		WHERE NOT EXISTS (SELECT * FROM answer WHERE project.id = answer.project_id AND answer.visitor_id = $visitor_id)
 		ORDER BY nb_answers, RAND()
 		LIMIT 1")->fetchAll()[0];
-
-	echo '<pre>';
-	var_dump($project);
-	echo '</pre>';
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>GitRank Survey</title>
+
+		<!-- Bootstrap core CSS -->
+		<link href="bootstrap-3.2.0/css/bootstrap.min.css" rel="stylesheet">
+		<link href="bootstrap-3.2.0/css/bootstrap-theme.min.css" rel="stylesheet">
+
+		<link href="sticky-footer.css" rel="stylesheet">
+
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+
+	<body>
+		<div class="container">
+			<div class="page-header">
+				<h1>
+					<a href="https://www.github.com/<?= $project['link_github'] ?>" target="_blank" >
+						<?= $project['link_github'] ?>
+					</a>
+				</h1>
+			</div>
+			<p class="lead">How maintainable is this project?</p>
+			<p>
+				Worst
+				<?php
+					for($i = 1; $i <= 5; $i++)
+						echo '<button type="button" class="btn btn-default">'.$i.'</button>
+					';
+				?>
+				Best
+			</p>
+		</div>
+		<div class="footer">
+			<div class="container">
+				<p class="text-muted">GitRank project.</p>
+			</div>
+		</div>
+	</body>
+</html>
