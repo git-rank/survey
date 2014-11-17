c09451f4cd8a863404bb0a5485a2f308e09e7ad2
Alexis Schad <schadoc_alex@hotmail.fr>
2014-11-18 00:40:22 +0100

    Modification des URLs

--- footer.php
+++ footer.php
@@ -2,7 +2,8 @@
 				<div class="container">
 					<p class="text-muted">This survey complements the GitRank project
 						realized by University of Technology of Compiègne's students.<br/>
-						<a href="survey.php" >Survey</a> - <a href="about_us.php" >About us</a></p>
+						<a href="Survey" >Survey</a>
+						- <a href="About-Us" >About us</a></p>
 				</div>
 			</div>
 	</body>
