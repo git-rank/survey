3d0ef698a0bacf9c96467a44375af2bfe1e280be
Alexis Schad <schadoc_alex@hotmail.fr>
2014-11-20 00:11:31 +0100

    Modification texte du footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 			<div class="footer">
 				<div class="container">
 					<p class="text-muted">This survey complements the GitRank project
-						realized by University of Technology of Compiègne's students.<br/>
+						realized by five students of Université de Technologie de Compiègne.<br/>
 						<a href="http://git-rank.net/Survey" >Survey</a>
 						- <a href="http://git-rank.net/About-Us" >About us</a></p>
 				</div>
